ef83c1bbd1c17afff82e2b31043fcc9febe5242f
om-ghante <omghante.official@gmail.com>
2025-01-26 17:58:56 +0530

    Check

--- TeamUp-server/index.js
+++ TeamUp-server/index.js
@@ -30,6 +30,12 @@ app.get('/', (req, res) => {
   res.send('Server Started');
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN_API);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 mongoose
   .connect(process.env.DATABASE_API, { useNewUrlParser: true, useUnifiedTopology: true })
