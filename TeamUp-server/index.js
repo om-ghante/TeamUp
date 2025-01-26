c2ff4c698b5f31fc23d1be58471cce9e31191b2b
om-ghante <omghante.official@gmail.com>
2025-01-26 15:11:39 +0530

    Cros Origin Bug Fixed

--- TeamUp-server/index.js
+++ TeamUp-server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.ORIGIN_API | 'http://localhost:5173',  
+    origin: process.env.ORIGIN_API || 'http://localhost:5173',  
     methods: ['GET', 'POST'], 
   }));
 
