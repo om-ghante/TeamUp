74fc0367cec0075dbf1302463853fc65fdf50c0d
om-ghante <omghante.official@gmail.com>
2025-01-26 15:23:55 +0530

    check

--- TeamUp-server/index.js
+++ TeamUp-server/index.js
@@ -2,28 +2,41 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');  
+const cors = require('cors');
+const helmet = require('helmet'); 
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const originAPI = process.env.ORIGIN_API || 'http://localhost:5173';
 
-app.use(cors({
-    origin: process.env.ORIGIN_API,  
-    methods: ['GET', 'POST'], 
-  }));
+app.use(
+  cors({
+    origin: originAPI,
+    methods: ['GET', 'POST'],
+  })
+);
+
+
+app.use(helmet());
+
 
 app.use(bodyParser.json());
 
+
 app.use('/api', authRoutes);
 app.get('/', (req, res) => {
-    res.send('Server Started');
+  res.send('Server Started');
 });
+
+
 mongoose
   .connect(process.env.DATABASE_API, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`,process.env.ORIGIN_API));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`CORS is enabled for: ${originAPI}`); 
+});
